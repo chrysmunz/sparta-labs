9fd6047cc068527ac6b2ea8442c6d33ac36c22c6
chrysmunz <chrys.munz@hotmail.com>
2021-12-16 18:36:02 -0300

    feat: :sparkles: created get current weather service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
+import { API_KEY } from '../config/constants';
+
 const api = axios.create({
   baseURL: 'http://api.openweathermap.org/data/2.5/',
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+export const getCurrentWeather = async (city) => {
+	return await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+		.then(response => {
+			return response.data;
+		})
+		.catch(error => {
+			return error;
+		});
+}
 
 export default api;
