120b96852e38abc13bb6b2e5c4c9832420fe19fa
chrysmunz <chrys.munz@hotmail.com>
2021-12-16 03:27:52 -0300

    feat: :sparkles: added theme provider

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled, { css } from 'styled-components/native';
+
+const StyledContainer = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.color.gray.secondary}
+    padding-horizontal: 16px;
+  `}
+`;
+
+const StyledEmptyTitle = styled.Text`
+  ${({ theme }) => css`
+    font-weight: ${theme.typography.weight.medium};
+    font-size: ${theme.typography.size.emptyTitle};
+    text-align: center;
+    margin-top: 62px;
+    color: ${theme.color.black};
+  `}
+`;
+
+const StyledEmptySubtitle = styled.Text`
+  ${({ theme }) => css`
+    font-weight: ${theme.typography.weight.regular};
+    font-size: ${theme.typography.size.h3};
+    text-align: center;
+    margin-top: 16px;
+    color: ${theme.color.gray.primary};
+  `}
+`;
+
+const Home: React.FC = () => {
+  return (
+    <StyledContainer>
+      <StyledEmptyTitle>Parece que você ainda não adicionou uma cidade</StyledEmptyTitle>
+      <StyledEmptySubtitle>Tente adicionar uma cidade usando o botão de busca</StyledEmptySubtitle>
+    </StyledContainer>
+  );
+};
+
+export default Home;
---
+++ src/styles/tokens/color.ts
@@ -0,0 +1,10 @@
+export default {
+  black: '#000000',
+  blue: '#00AAD2',
+  gray: {
+    primary: '#202020',
+    secondary: '#FAFAFA'
+  },
+  orange: '#F28200',
+  white: '#FFFFFF'
+} as const;
