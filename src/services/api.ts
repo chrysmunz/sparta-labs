03f68b9dc7c82173586f2513c438d792fa00a21e
chrysmunz <chrys.munz@hotmail.com>
2021-12-17 16:27:00 -0300

    refactor: :recycle: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,14 +3,20 @@ import axios from 'axios';
 import { API_KEY } from '../config/constants';
 
 const api = axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5/',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+	baseURL: 'http://api.openweathermap.org/data/2.5/',
+	headers: {
+		'Content-Type': 'application/json'
+	}
 });
 
-export const getCurrentWeather = async (city) => {
-	return await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+export const getCurrentWeather = async ({ lat, lon }) => {
+	return await axios.get(`http://api.openweathermap.org/data/2.5/onecall?
+		lang=pt
+		&units=metric
+		&lat=${lat}
+		&lon=${lon}
+		&exclude=minutely,hourly,alerts
+		&appid=${API_KEY}`)
 		.then(response => {
 			return response.data;
 		})
